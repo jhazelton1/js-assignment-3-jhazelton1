205ae2fa6d1968e0d5e4979bf1f5660d20f28e5e
Hazelton Jacob <jhazelton11@gmail.com>
2018-08-30 22:04:26 -0500

    JS Assignment 3 Revision and Final Optional Task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // TODO import readFile, writeFile, chooseRandom, createPrompt, and createQuestions
-import { createPrompt, chooseRandom } from './lib.js'
-import { createQuestions } from './lib.js'
+import { createPrompt, chooseRandom, createQuestions } from './lib.js'
+
 const fs = require('fs')
 
 // TODO read in the file createPrompt.json, run the object read in through createPrompt,
@@ -19,9 +19,9 @@ let reader = JSON.parse(
   )
 )
 
-let createdPrompt = createPrompt(reader)
+let createdPrompts = createPrompt(reader)
 
-let randomized = chooseRandom(createdPrompt, 10)
+let randomized = chooseRandom(createdPrompts, 10)
 fs.writeFileSync(
   '/Users/jacobhazelton/repos/vs/js-assignment-3-jhazelton1/lib/cp_solutions.json',
   JSON.stringify(randomized),
@@ -36,3 +36,31 @@ fs.writeFileSync(
 
 // TODO read the file createQuestions.json. Pass the read in object to createQuestions,
 // pass its result to chooseRandom, and then save the final result to cq_solutions.json
+
+let questionsReader = JSON.parse(
+  fs.readFileSync(
+    '/Users/jacobhazelton/repos/vs/js-assignment-3-jhazelton1/lib/createQuestions.json',
+    'utf8',
+    err => {
+      if (err) {
+        console.log(err)
+      }
+      console.log('Second File Read')
+    }
+  )
+)
+
+let createdQuestions = createQuestions(questionsReader)
+
+let randomizedQuestions = chooseRandom(createdQuestions, 5)
+fs.writeFileSync(
+  '/Users/jacobhazelton/repos/vs/js-assignment-3-jhazelton1/lib/cq_solutions.json',
+  JSON.stringify(randomizedQuestions),
+  'utf8',
+  err => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Success! Again!')
+  }
+)
